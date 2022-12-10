ed0dea5b73040efb0de47a09e94ded9b0e013a2e
Kreedzt <zhaozisong1@live.com>
2022-12-10 18:35:41 +0800

    :sparkles: Kreedzt - add rust sqlite lib
    add zhihu site ref

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 const TARGET_URL: &str = "http://rwr.runningwithrifles.com/rwr_stats/view_players.php";
 
+// ref: https://zhuanlan.zhihu.com/p/516033159
 fn main() {
     println!("Target url: {}", TARGET_URL);
 }
