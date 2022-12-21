5b259dba382bb6d743c85d767f026e54c85816df
Kreedzt <zhaozisong1@live.com>
2022-12-21 10:40:05 +0800

    :refactor: Kreedzt - extract table name

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,16 @@
-use std::collections::{HashMap, HashSet};
-
 use anyhow::Result as AnyhowResult;
 use reqwest;
+use rusqlite::{Connection, Result as RSqlResult};
 use scraper::{Html, Selector};
+use std::collections::{HashMap, HashSet};
 
 const TARGET_URL: &str = "http://rwr.runningwithrifles.com/rwr_stats/view_players.php";
 const SELECTOR_MATCH: &str = "table > tbody > tr";
 const PAGE_SIZE: u8 = 100;
+const DB_NAME: &str = "rwr_players.db";
+const TABLE_NAME: &str = "rwr_players";
 
-const DROP_TABLE_SQL: &str = "DROP TABLE IF EXISTS rwr_players";
-
-const CREATE_TABLE_SQL: &str = "CREATE TABLE \"rwr_players\" (
-	\"id\" INTEGER NOT NULL,
-	\"username\" TEXT NOT NULL,
-	\"kills\" BIGINT NOT NULL,
-	\"deaths\" BIGINT NOT NULL,
-	\"score\" BIGINT NOT NULL,
-	\"time_played\" BIGINT NOT NULL,
-	\"longest_kill_streak\" BIGINT NOT NULL,
-	\"targets_destroyed\" BIGINT NOT NULL,
-	\"soldiers_healed\" BIGINT NOT NULL,
-	\"teamkills\" BIGINT NOT NULL,
-	\"distance_moved\" REAL NOT NULL,
-	\"shots_fired\" BIGINT NOT NULL,
-	\"throwables_thrown\" BIGINT NOT NULL,
-	\"rank_progression\" BIGINT NOT NULL,
-	\"rank_name\" TEXT NOT NULL,
-	PRIMARY KEY (\"id\")
-);";
-
+#[derive(Debug)]
 struct Player {
     username: String,
     kills: i128,
@@ -66,25 +48,85 @@ impl Default for Player {
             shots_fired: 0,
             throwables_thrown: 0,
             rank_progression: 0,
-            rank_name: "".to_string()
+            rank_name: "".to_string(),
         }
     }
+}
+
+fn get_drop_table_sql(table_name: &str) -> String {
+    format!("DROP TABLE IF EXISTS {}", table_name)
+}
+
+fn get_create_table_sql(table_name: &str) -> String {
+    format!(
+        "CREATE TABLE \"{}\" (
+	\"id\" INTEGER NOT NULL,
+	\"username\" TEXT NOT NULL,
+	\"kills\" BIGINT NOT NULL,
+	\"deaths\" BIGINT NOT NULL,
+	\"score\" BIGINT NOT NULL,
+	\"time_played\" BIGINT NOT NULL,
+	\"longest_kill_streak\" BIGINT NOT NULL,
+	\"targets_destroyed\" BIGINT NOT NULL,
+	\"soldiers_healed\" BIGINT NOT NULL,
+	\"teamkills\" BIGINT NOT NULL,
+	\"distance_moved\" REAL NOT NULL,
+	\"shots_fired\" BIGINT NOT NULL,
+	\"throwables_thrown\" BIGINT NOT NULL,
+	\"rank_progression\" BIGINT NOT NULL,
+	\"rank_name\" TEXT NOT NULL,
+	PRIMARY KEY (\"id\")
+);",
+        table_name
+    )
 }
 
 fn quick_selector(exp: &str) -> Selector {
     Selector::parse(exp).unwrap()
 }
 
+fn insert_player_data(conn: &Connection, player: Player) -> RSqlResult<usize> {
+    let sql_text = format!(
+        "INSERT INTO rwr_players (username, kills, deaths, score,time_played,longest_kill_streak,targets_destroyed,soldiers_healed,teamkills,distance_moved,shots_fired,throwables_thrown,rank_progression,rank_name)
+VALUES('{}',{},{},{},{},{},{},{},{},{},{},{},{},'{}')",
+                           player.username,
+                           player.kills,
+                           player.deaths,
+                           player.score,
+                           player.time_played,
+                           player.longest_kill_streak,
+                           player.targets_destroyed,
+                           player.soldiers_healed,
+                           player.teamkills,
+                           player.distance_moved,
+                           player.shots_fired,
+                           player.throwables_thrown,
+                           player.rank_progression,
+                           player.rank_name
+    );
+
+    conn.execute(&sql_text, ())
+}
+
 // ref: https://zhuanlan.zhihu.com/p/516033159
 fn main() -> AnyhowResult<()> {
+    println!("Creating SQLite connection...");
+    let conn = Connection::open(DB_NAME)?;
+
+    println!("Dropping SQLite Table...");
+    conn.execute(&get_drop_table_sql(TABLE_NAME), ())?;
+
+    println!("Creating SQLite Table...");
+    conn.execute(&get_create_table_sql(TABLE_NAME), ())?;
+
     println!("Target url: {}", TARGET_URL);
 
     let client = reqwest::blocking::Client::new();
 
     // End: 148900
     // No data: 149000
-    let mut current_start = 148800;
-    // let mut current_start = 0;
+    // let mut current_start = 148800;
+    let mut current_start = 0;
 
     loop {
         let resp = client
@@ -105,7 +147,10 @@ fn main() -> AnyhowResult<()> {
         let mut data_size: i128 = -1;
 
         for element in fragment.select(&selector) {
-            println!("Start Parsing... start:{}, data(before):{}", current_start, data_size);
+            println!(
+                "Start Parsing... start:{}, data(before):{}",
+                current_start, data_size
+            );
             // println!("tr element: {:?}", element.value());
 
             // column name
@@ -126,7 +171,6 @@ fn main() -> AnyhowResult<()> {
 
             // data
             for (index, td) in element.select(&quick_selector("td")).enumerate() {
-
                 match td.text().next() {
                     Some(t) => {
                         let key = property_map.iter().nth(index);
@@ -153,8 +197,26 @@ fn main() -> AnyhowResult<()> {
                                     println!("score: {}", t);
                                 }
                                 "time_played" => {
-                                    // TODO
-                                    // player.time_played = t.parse()?;
+                                    // Example source str: 1718h 48min
+                                    let times_str = t.replace("h ", "|").replace("min", "");
+                                    let times_str_split_collect = times_str
+                                        .split("|")
+                                        .map(|s| s.to_string())
+                                        .collect::<Vec<String>>();
+                                    let times_str_iter = times_str_split_collect.iter().rev();
+
+                                    let mut times_by_minute = 0;
+
+                                    for (index, time_item) in times_str_iter.enumerate() {
+                                        let v = time_item.parse::<i128>()?;
+                                        if index == 0 {
+                                            times_by_minute = times_by_minute + v;
+                                        } else if index == 1 {
+                                            times_by_minute = times_by_minute + v * 60;
+                                        }
+                                    }
+
+                                    player.time_played = times_by_minute;
                                     println!("time_played: {}", t);
                                 }
                                 "longest_kill_streak" => {
@@ -174,8 +236,8 @@ fn main() -> AnyhowResult<()> {
                                     println!("teamkills: {}", t);
                                 }
                                 "distance_moved" => {
-                                    // TODO
-                                    // player.teamkills = t.parse()?;
+                                    let distance_str = t.replace("km", "");
+                                    player.distance_moved = distance_str.parse()?;
                                     println!("distance_moved: {}", t);
                                 }
                                 "shots_fired" => {
@@ -206,10 +268,20 @@ fn main() -> AnyhowResult<()> {
                 }
             }
 
+            if data_size != -1 {
+                println!("Player Parse:");
+                println!("{:?}", player);
+
+                insert_player_data(&conn, player)?;
+            }
+
             data_size = data_size + 1;
         }
 
-        println!("Parsing completed, start:{}, data(after):{}", current_start, data_size);
+        println!(
+            "Parsing completed, start:{}, data(after):{}",
+            current_start, data_size
+        );
 
         if data_size < PAGE_SIZE.into() {
             println!("Parsing End");
@@ -222,6 +294,8 @@ fn main() -> AnyhowResult<()> {
         }
 
         current_start = current_start + PAGE_SIZE as i128;
+
+        break;
     }
 
     Ok(())
